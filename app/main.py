b7c1e510a6a0f3ab3ab21515d0939ad750dd4139
anton <antonkuzmn1@gmail.com>
2025-02-09 23:42:06 +0700

    added companies router

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 from app.api.admins import router as admins_router
+from app.api.companies import router as companies_router
 from app.config import settings
 
 app = FastAPI()
 
 app.include_router(admins_router)
+app.include_router(companies_router)
 
 
 @app.get("/")
