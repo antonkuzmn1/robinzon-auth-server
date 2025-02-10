9909908e5ed75fbac8849c0610d21f9b4f59bdb9
anton <antonkuzmn1@gmail.com>
2025-02-11 04:33:50 +0700

    ci cd test pt11

--- app/main.py
+++ app/main.py
@@ -1,23 +1,18 @@
 from fastapi import FastAPI
-# from app.api.admins import router as admins_router
-# from app.api.companies import router as companies_router
-# from app.api.users import router as users_router
+from app.api.admins import router as admins_router
+from app.api.companies import router as companies_router
+from app.api.users import router as users_router
 from app.config import settings
 
-print(settings)
-print(type(settings))
-print(f"app before init: {globals().get('app', None)}")
 app = FastAPI()
-print(f"app after init: {type(app)}")
 
-# app.include_router(admins_router)
-# app.include_router(companies_router)
-# app.include_router(users_router)
+app.include_router(admins_router)
+app.include_router(companies_router)
+app.include_router(users_router)
 
 
 @app.get("/")
 def main():
-    print(f"settings is of type: {type(settings)}")
     return {
         "message": "test!",
         "debug": settings.DEBUG,
