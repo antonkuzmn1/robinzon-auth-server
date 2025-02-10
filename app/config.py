b1659652ada50fe3e11a875ca28be69e37d6d952
JohnLool <pinaev-misha-pinaev@yandex.ru>
2025-02-10 23:35:51 +0500

    config updated, alembic added

--- app/config.py
+++ app/config.py
@@ -1,12 +1,21 @@
-import os
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
-from dotenv import load_dotenv
 
-load_dotenv()
+class Settings(BaseSettings):
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
 
-class Settings:
-    DATABASE_URL: str = os.getenv("DATABASE_URL")
-    SECRET_KEY: str = os.getenv("SECRET_KEY")
-    DEBUG: bool = os.getenv("DEBUG", "False").lower() in ("true", "1", "t")
+    SECRET_KEY: str
+    DEBUG: bool = False
+
+    @property
+    def database_url(self):
+        return f"mysql+pymysql://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    model_config = SettingsConfigDict(env_file=".env")
+
 
 settings = Settings()
